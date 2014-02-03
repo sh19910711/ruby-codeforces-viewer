bd5732180bd3085bad2c733c82a48fd99f572195
Hiroyuki Sano <sh19910711@gmail.com>
2014-02-03 21:58:51 +0900

    Initial commit

---
+++ lib/codeforces/viewer.rb
@@ -0,0 +1,7 @@
+require "codeforces/viewer/version"
+
+module Codeforces
+  module Viewer
+    # Your code goes here...
+  end
+end
---
+++ lib/codeforces/viewer/version.rb
@@ -0,0 +1,5 @@
+module Codeforces
+  module Viewer
+    VERSION = "0.0.1"
+  end
+end
